67277c69fa4edb3ed38cd2a855549146ba5fe7ec
toyohide <hide.toyoda@gmail.com>
2023-10-21 09:53:43 +0900

    commit 20231021 ボトムナビゲーション作成

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,21 +1,49 @@
 import 'package:flutter/material.dart';
 
 import '../generated/l10n.dart';
+import 'pages/activity_page.dart';
+import 'pages/feed_page.dart';
+import 'pages/post_page.dart';
+import 'pages/profile_page.dart';
+import 'pages/search_page.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<Widget> _pages = [];
+  int _currentIndex = 0;
+
+  ///
+  @override
+  void initState() {
+    _pages = [const FeedPage(), const SearchPage(), const PostPage(), const ActivityPage(), const ProfilePage()];
+
+    super.initState();
+  }
 
   ///
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            Text(S.of(context).appTitle),
-            const Text('HomeScreen'),
-          ],
-        ),
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        currentIndex: _currentIndex,
+        onTap: (index) => setState(() => _currentIndex = index),
+        items: [
+          BottomNavigationBarItem(icon: const Icon(Icons.home), label: S.of(context).home),
+          BottomNavigationBarItem(icon: const Icon(Icons.search), label: S.of(context).search),
+          BottomNavigationBarItem(icon: const Icon(Icons.add), label: S.of(context).add),
+          BottomNavigationBarItem(icon: const Icon(Icons.favorite), label: S.of(context).activities),
+          BottomNavigationBarItem(icon: const Icon(Icons.person), label: S.of(context).user),
+        ],
       ),
     );
   }
---
+++ lib/screens/pages/post_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class PostPage extends StatelessWidget {
+  const PostPage({super.key});
+
+  ///
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Text('PostPage'),
+          ],
+        ),
+      ),
+    );
+  }
+}
