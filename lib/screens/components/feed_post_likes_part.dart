351ed05a472e3e62459c4d3ddc92aa01b63a006e
toyohide <hide.toyoda@gmail.com>
2023-10-22 21:03:58 +0900

    commit 20231022 いいねパート作成

--- lib/screens/components/feed_post_likes_part.dart
+++ lib/screens/components/feed_post_likes_part.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
 
+import '../../generated/l10n.dart';
+
 class FeedPostLikesPart extends StatelessWidget {
   const FeedPostLikesPart({super.key});
 
   ///
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('FeedPostLikesPart'),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.favorite),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.comment),
+              ),
+            ],
+          ),
+          Text('0 ${S.of(context).likes}'),
+        ],
+      ),
     );
   }
 }
